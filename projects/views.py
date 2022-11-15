d522b1d439a8eb55a9620ff465c05c5ab5ca00fc
Jack112-create <jackconroy777@gmail.com>
2022-11-15 20:54:47 +0000

    Database Queries - Query Projects model

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
 def projects(request):
-    return render(request, 'projects/projects.html')
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    context = {
+        'project': projectObj,
+        'tags': tags
+    }
+    return render(request, 'projects/single-project.html', context)
