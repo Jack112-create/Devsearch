8e2e9fa9e43760681d87cae7d5f7db9917042d7b
Jack112-create <jackconroy777@gmail.com>
2022-12-13 16:15:11 +0000

    Add edit user profile form

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Profile, User
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 def profiles(request):
   profiles = Profile.objects.all()
@@ -70,7 +70,7 @@ def registerUser(request):
       messages.success(request, 'User account was created!')
 
       login(request, user)
-      return redirect('profiles')
+      return redirect('account')
 
     else:
       messages.error(request, 'An error has occurred during registration')
@@ -93,3 +93,19 @@ def userAccount(request):
     'projects': projects,
     }
   return render(request, 'users/account.html', context)
+
+@login_required(login_url='login')
+def editAccount(request):
+  profile = request.user.profile
+  form = ProfileForm(instance=profile)
+
+  if request.method == 'POST':
+    form = ProfileForm(request.POST, request.FILES, instance=profile)
+    if form.is_valid():
+      form.save()
+      return redirect('account')
+
+  context = {
+    'form': form
+  }
+  return render(request, 'users/profile_form.html', context)
