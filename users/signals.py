588d32d40c9b58a69bcbc1b78a1981a865a1e8be
Jack112-create <jackconroy777@gmail.com>
2023-01-14 19:43:52 +0000

    Setup email sending functionality using Django send mail

--- users/signals.py
+++ users/signals.py
@@ -4,14 +4,28 @@ from .models import User, Profile
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 def create_profile(sender, instance, created, **kwargs):
   if created:
     user = instance
-    Profile.objects.create(
+    profile = Profile.objects.create(
       user=user,
       username=user.username,
       email=user.email,
       name=user.first_name,
+    )
+
+    subject = 'Welcome to Devsearch.'
+    message = 'We are glad you are here.'
+
+    send_mail(
+      subject,
+      message,
+      settings.EMAIL_HOST_USER,
+      [profile.email],
+      fail_silently=False
     )
 
 def updateUser(sender, instance, created, **kwargs):
